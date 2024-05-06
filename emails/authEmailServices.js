7d2d72c2ac15713d08436b0b51f850c812d8de5c
puly garcia <pulygarcia09@gmail.com>
2024-05-06 16:28:03 -0300

    redirection route in verification email was changed

--- emails/authEmailServices.js
+++ emails/authEmailServices.js
@@ -2,10 +2,10 @@ import { createTransport } from "../config/nodemailer.js";
 
 export const sendVerificationEmail = async (userData) => {
     const transporter = createTransport(
-        "sandbox.smtp.mailtrap.io",
-        2525,
-        "d0e16083c53773",
-        "323db990eef951"
+        process.env.MAILTRAP_HOST,
+        process.env.MAILTRAP_PORT,
+        process.env.MAILTRAP_USER,
+        process.env.MAILTRAP_PASSWORD
     );
 
     return await transporter.sendMail({
@@ -15,7 +15,7 @@ export const sendVerificationEmail = async (userData) => {
         text: "make a click to verify your account", // plain text body
         html: `
             <p>Please verify your account ${userData.email} so that you can enter to the control panel</p>
-            <a href="http://localhost:4000/api/auth/verify/${userData.token}">Click to confirm account</a>
+            <a href="${process.env.FRONTEND_URL}/auth/verify/${userData.token}">Click to confirm account</a>
         `
     });
 }
